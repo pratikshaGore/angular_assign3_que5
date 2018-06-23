a7b5cd768c56be05f1a3a52e67b5fb781c2768fd
pratikshaGore <pratikshagore27@gmail.com>
2018-06-23 08:41:54 +0530

    assignment third que 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Pune';
+  name="Marvellous Infosystems";
+  public fun()
+  {
+this.name="Button Clicked";
+  }
 }
